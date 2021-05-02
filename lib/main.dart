3090fedf213ca07d3ddfd8458456d08cac99f55d
jamescardona11 <jjcardonao@gmail.com>
2021-05-02 11:57:50 -0500

    Init project for landing page The Koding Monkey

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: SvgPicture.asset('assets/images/logo.svg'),
+        ),
+      ),
+    );
+  }
+}
